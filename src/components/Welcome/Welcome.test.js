f70e266457032bd7c7956d19993a9a8728849945
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-09-11 18:16:16 -0300

    refactor

--- src/components/Welcome/Welcome.test.js
+++ src/components/Welcome/Welcome.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { create } from 'react-test-renderer'
 
 import Welcome from './'
 
 describe('Welcome', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<Welcome />).toJSON();
+    const tree = create(<Welcome />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 })
