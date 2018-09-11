dc86de4b7307da842e9b90e9ce59a1c363a5ab2a
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-09-11 17:36:40 -0300

    tests: Add snapshot component

---
+++ src/components/TextInputField/TextInputField.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+
+import TextInputField from './';
+
+describe('TextInputField', () => {
+  const onChangeText = jest.fn();
+  const tree = create(
+    <TextInputField
+    placeholder="something"
+    maxLength={30}
+    fieldLabel="Enter something"
+    onChangeText={onChangeText}
+    />
+  );
+  
+  it('renders correctly', () => {
+    expect(tree.toJSON()).toMatchSnapshot()
+  });
+});
