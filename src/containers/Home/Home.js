a626fdbe6fd1724fe32e514344c2f86111d373f9
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-09-18 05:12:39 -0300

    refactor: class component to functional component

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,19 +1,16 @@
-import React, { Component } from "react";
-import { View } from "react-native";
+import React from "react";
 
 import { Sector } from "../../config/theme";
 
 import Header from "../../components/Header";
 import ListDeck from "../../containers/Decks/ListDeck";
 
-export default class HomeScreen extends Component {
-  render() {
-    return (
-      <Sector>
-        <Header title="Welcome" subtitle="Memorize anything" />
+const HomeScreen = ({ navigation }) => (
+  <Sector>
+    <Header title="Welcome" subtitle="Memorize anything" />
 
-        <ListDeck navigation={this.props.navigation} />
-      </Sector>
-    );
-  }
-}
+    <ListDeck navigation={navigation} />
+  </Sector>
+);
+
+export default HomeScreen;
